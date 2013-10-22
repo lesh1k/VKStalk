6d1c21a488a4aea622e63a56f7522bf38998ae1f
Alexei Dvorac <lexhxas@gmail.com>
2013-10-23 01:03:51 +0300

    compiled vkstalk

--- vkstalk/mainOOP.py
+++
@@ -1,7 +0,0 @@
-from vkstalk import VKStalk
-
-if __name__ == "__main__":
-	#Keyboard input of user ID
-	user_ID = raw_input('User ID:') #e.g."83029348"
-	vk_object = VKStalk(user_ID)
-	vk_object.work()
