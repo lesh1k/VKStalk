370225d9cc501c21e51cb36c15621f37b815f553
Alexei Dvorac <lexhxas@gmail.com>
2015-11-04 00:56:57 +0200

    Add secrets_sample. Refactor parser.py.

---
+++ src/config/secrets_sample.py
@@ -0,0 +1,9 @@
+DATABASE = {
+    # Replace with database backend which you'll use
+    'drivername': 'postgres',
+    'database': 'DB_NAME',
+    'host': '127.0.0.1',
+    'port': 5432,
+    'username': 'DB_USER',
+    'password': 'DB_PASSWORD'
+}
--- src/core/parser.py
+++ src/core/parser.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 from bs4 import BeautifulSoup
 from datetime import datetime, timedelta
-from helpers.utils import get_all_digits_from_str
+from helpers.utils import get_all_digits_from_str, clear_screen
 from helpers.h_logging import get_logger
 from config import settings
 
@@ -12,16 +12,17 @@ import sys
 import pytz
 import socket
 import time
+import re
 
 
 class Parser:
     def __init__(self, url):
         self.user = {}
         self.url = url
-        html = self.fetch_html()
-        self.soup = self.cook_soup(html)
+        self.soup = self.cook_soup()
 
-    def cook_soup(self, html):
+    def cook_soup(self):
+        html = self.fetch_html()
         soup = None
         if html:
             soup = BeautifulSoup(html)
@@ -43,11 +44,16 @@ class Parser:
                 get_logger('file').error(
                     'Connection timed out')
             except urllib2.URLError, e:
-                if e.errno and e.errno == 101 or isinstance(e.reason, socket.gaierror):
-                    message = 'Attempt {0}. Network unavailable. Retry in {1} seconds...'.format(attempt, settings.DATA_FETCH_INTERVAL)
+                if e.errno and e.errno == 101 \
+                   or isinstance(e.reason, socket.gaierror):
+                    message = 'Attempt {0}. Network unavailable.'
+                    message += ' Retry in {1} seconds...'
+                    message = message.format(attempt,
+                                             settings.DATA_FETCH_INTERVAL)
                     get_logger('file').error(message)
                     get_logger('console').info(message)
                     time.sleep(settings.DATA_FETCH_INTERVAL)
+                    clear_screen()
                 else:
                     raise
             attempt += 1
@@ -80,131 +86,123 @@ class Parser:
         return self.user
 
     def is_profile_private(self):
-        max_attempts = settings.MAX_CONNECTION_ATTEMPTS
-        for attempt in xrange(1, max_attempts + 1):
-            if ((self.soup.find('div', {'class': 'service_msg_null'}))
-                    or ('This user deleted their page. Information unavailable.' in self.soup.text)
-                    or ('This page is either deleted or has not been created yet.' in self.soup.text)):
+        text_to_find = [
+            'You have to log in in order to view this page.',
+            'This user deleted their page. Information unavailable.',
+            'This page is either deleted or has not been created yet.',
+        ]
+        for attempt in xrange(settings.MAX_CONNECTION_ATTEMPTS):
+            if self.soup(class_="service_msg_null", string=text_to_find):
+                clear_screen()
+                message = 'User profile private, deleted or does not exist.'
+                message += ' Attempt {0} of {1}. Retry in {2} seonds...'
+                message = message.format(attempt+1,
+                                         settings.MAX_CONNECTION_ATTEMPTS,
+                                         settings.DATA_FETCH_INTERVAL)
+                get_logger('file').error(message)
+                get_logger('console').error(message)
+                time.sleep(settings.DATA_FETCH_INTERVAL)
                 self.cook_soup()
                 profile_private = True
             else:
-                # self.vk_logger.logger.debug('Profile PUBLIC. OK!')
+                get_logger('file').info('Profile PUBLIC. OK!')
                 profile_private = False
                 break
 
         return profile_private
 
     def get_user_name(self):
-        try:
-            username = self.soup.html.head.title.text
-            username = username.rstrip(" VK")
-        except Exception as e:
-            print "Error in '{}'".format(sys._getframe().f_code.co_name)
+        username = self.soup.html.head.title.text
+        username = username.rstrip(" VK")
         return username
 
     def get_user_status(self):
-        # self.vk_logger.logger.debug('Obtaining user status')
-        status = self.soup.find('div', {"class": "status"})
+        status = self.soup(class_=["status", "pp_status"])
         if status:
-            status_text = status.text
+            status_text = status[0].text
         else:
-            status = self.soup.find('div', {'class': 'pp_status'})
-            if status:
-                status_text = status.text
-            else:
-                status_text = None
+            status_text = None
         return status_text
 
     def is_user_mobile(self):
         is_mobile = False
-        try:
-            if self.soup.find(class_='mlvi') is not None:
-                is_mobile = True
-            return is_mobile
-        except Exception as e:
-            print "Error in '{}'".format(sys._getframe().f_code.co_name)
+        if self.soup.find(class_='mlvi') is not None:
+            is_mobile = True
+        return is_mobile
 
     def is_user_online(self):
-        # Assuming that the user is online, unless last_seen text is found
-        is_online = True
-        try:
-            last_seen = self.get_user_last_seen_text()
-            if last_seen and last_seen != '':
-                is_online = False
-                if last_seen.lower() == 'online':
-                    is_online = True
-
-        except Exception as e:
-            print "Error in '{}'".format(sys._getframe().f_code.co_name)
-
+        last_seen = self.get_user_last_seen_text()
+        if last_seen and last_seen.lower() == "online":
+            is_online = True
+        else:
+            is_online = False
         return is_online
 
     def get_user_last_seen_text(self):
-        text = ""
-        last_seen = self.soup.find('div', {'class': 'lv'})
+        last_seen = self.soup(class_=["lv", "pp_last_activity", "profile_time_lv"])
         if last_seen:
-            text = last_seen.text
+            text = last_seen[0].text
         else:
-            last_seen = self.soup.find(
-                'div', {'class': 'pp_last_activity'})
-            if last_seen:
-                text = last_seen.text
-            else:
-                last_seen = self.soup.find('b', {'id': 'profile_time_lv'})
-                if last_seen:
-                    text = last_seen.text
+            text = ""
         return text
 
     def get_last_seen_datetime(self):
-        try:
-            last_seen = self.get_user_last_seen_text()
+        if self.is_user_online():
+            return None
 
-            if not last_seen or last_seen.lower() == "online":
-                return None
+        last_seen = self.get_user_last_seen_text()
 
-            if 'today' in last_seen or 'yesterday' in last_seen:
-                if 'today' in last_seen:
-                    dt = datetime.today()
-                else:
-                    dt = datetime.fromordinal(datetime.today().toordinal()-1)
-                last_seen_time = datetime.strptime(
-                    last_seen[last_seen.index("at"):],
-                    "at %I:%M %p")
-                dt = dt.replace(hour=last_seen_time.hour,
-                                minute=last_seen_time.minute)
-            elif 'ago' in last_seen:
-                dt = datetime.now()
-                last_seen_minutes_ago = int(get_all_digits_from_str(last_seen))
-                dt = dt - timedelta(minutes=last_seen_minutes_ago)
+        if 'today' in last_seen or 'yesterday' in last_seen:
+            if 'today' in last_seen:
+                dt = datetime.today()
             else:
-                dt = datetime.strptime(last_seen, "last seen %d %B at %I:%M %p")
+                dt = datetime.fromordinal(datetime.today().toordinal()-1)
+            last_seen_time = datetime.strptime(
+                last_seen[last_seen.index("at"):], "at %I:%M %p")
+            dt = dt.replace(hour=last_seen_time.hour,
+                            minute=last_seen_time.minute)
+        elif 'ago' in last_seen:
+            dt = datetime.now()
+            last_seen_minutes_ago = int(get_all_digits_from_str(last_seen))
+            dt = dt - timedelta(minutes=last_seen_minutes_ago)
+        else:
+            dt = datetime.strptime(last_seen, "last seen %d %B at %I:%M %p")
 
-            year = datetime.now().year
-            dt = dt.replace(year=year, second=0, microsecond=0)
-            if 'ago' in last_seen:
-                dt = pytz.timezone(settings.CLIENT_TZ).localize(dt)
-            else:
-                dt = pytz.timezone(settings.VK_TZ).localize(dt)
-            dt = dt.astimezone(pytz.timezone(settings.CLIENT_TZ))
-        except Exception as e:
-            print "Error in '{}'".format(sys._getframe().f_code.co_name)
+        year = datetime.now().year
+        dt = dt.replace(year=year, second=0, microsecond=0)
+        if 'ago' in last_seen:
+            dt = pytz.timezone(settings.CLIENT_TZ).localize(dt)
+        else:
+            dt = pytz.timezone(settings.VK_TZ).localize(dt)
+        dt = dt.astimezone(pytz.timezone(settings.CLIENT_TZ))
         return dt
 
     def get_user_secondary_data(self):
         secondary_data = {}
-        try:
-            secondary_data.update(self.get_user_additional_info())
-            secondary_data.update(self.get_user_extra_data())
-            secondary_data["wallposts"] = self.get_user_number_of_wallposts()
-            secondary_data["photo"] = self.get_user_profile_photo_link()
-        except Exception as e:
-            print "Error in '{}'".format(sys._getframe().f_code.co_name)
+        secondary_data["wallposts"] = self.get_user_number_of_wallposts()
+        secondary_data["photo"] = self.get_user_photo_url()
+        additional_info = self.get_user_additional_info()
+        secondary_data.update(additional_info)
         return secondary_data
 
     def get_user_additional_info(self):
         additional_info = {}
+
+        info_from_pinfo_row = self.get_user_info_from_pinfo_row()
+        additional_info.update(info_from_pinfo_row)
+
+        info_from_pp_info = self.get_user_info_from_pp_info()
+        additional_info.update(info_from_pp_info)
+
+        info_from_profile_menu = self.get_user_info_from_profile_menu()
+        additional_info.update(info_from_profile_menu)
+
+        return additional_info
+
+    def get_user_info_from_pinfo_row(self):
+        info = {}
         short_profile_info = []
-        for item in self.soup.findAll(class_='pinfo_row'):
+        for item in self.soup(class_='pinfo_row'):
             text = item.text
             if ':' in text:
                 short_profile_info.append(text)
@@ -212,9 +210,14 @@ class Parser:
         for info_item in short_profile_info:
             item_title, item_value = info_item.split(":")
             item_title = item_title.lower().replace(" ", "_").strip()
-            additional_info[item_title] = item_value
+            info[item_title] = item_value
 
-        for i, item in enumerate(self.soup.findAll(class_='pp_info')):
+        return info
+
+    def get_user_info_from_pp_info(self):
+        info = {}
+        short_profile_info = []
+        for i, item in enumerate(self.soup(class_='pp_info')):
             text = item.text
             tmp = "Info {0}: {1}".format(i+1, text)
             short_profile_info.append(tmp)
@@ -222,57 +225,40 @@ class Parser:
         for info_item in short_profile_info:
             item_title, item_value = info_item.split(":")
             item_title = item_title.lower().replace(" ", "_").strip()
-            additional_info[item_title] = item_value
+            info[item_title] = item_value
 
-        return additional_info
+        return info
 
-    def get_user_extra_data(self):
-        # self.vk_logger.logger.debug(
-        #     "Getting extra data (e.g. number of photos/communities)")
+    def get_user_info_from_profile_menu(self):
         data = {}
-        extra_data = self.soup.findAll(class_='profile_menu')
-        if extra_data:
-            extra_data_list = []
-            tmp_list = []
-            for el in extra_data:
-                for item in el.findAll(class_='pm_item'):
-                    item = item.text.lower()
-                    if 'show more' not in item:
-                        extra_data_list.append(item)
+        extra_data = self.soup.select('.profile_menu .pm_item')
+        extra_data_list = []
+        for item in extra_data:
+            item = item.text.lower()
+            if 'show more' not in item:
+                extra_data_list.append(item)
 
-        # self.vk_logger.logger.debug(
-        #     "Parsing extra data (e.g. number of photos/communities)")
         for item in extra_data_list:
-            item_parts = item.split(' ')
-            key = ''
-            value = ''
-            for part in item_parts:
-                if part.isdigit():
-                    value = part
-                else:
-                    key += part + '_'
-            key = key.replace('_', ' ').rstrip().replace(' ', '_')
-            key = key.lower().replace(" ", "_").strip()
+            item_parts = item.split()
+            value = get_all_digits_from_str(item)
+            key = '_'.join([i.lower() for i in item_parts if i != value])
             data[key] = value
 
         return data
 
     def get_user_number_of_wallposts(self):
-        # self.vk_logger.logger.debug("Getting number of wall posts")
-        wallposts_number = -1
-        all_slim_headers = self.soup(class_='slim_header')
-        if len(all_slim_headers) > 0:
-            for item in all_slim_headers:
-                if 'post' in item.text:
-                    number_of_wallposts = get_all_digits_from_str(item.text)
-                    if str(number_of_wallposts).isdigit():
-                        wallposts_number = number_of_wallposts
-                    else:
-                        break
+        matching_items = self.soup(class_='slim_header',
+                                   text=re.compile("\d+ post."))
+        if matching_items:
+            wallposts_number = get_all_digits_from_str(matching_items[0].text)
+        if wallposts_number.isdigit():
+            wallposts_number = int(wallposts_number)
+        else:
+            wallposts_number = -1
         return wallposts_number
 
-    def get_user_profile_photo_link(self):
-        get_logger('file').debug('Getting link to profile photo.')
+    def get_user_photo_url(self):
+        get_logger('file').debug('Getting profile photo URL.')
         photo_url = None
         photo = self.soup.select("#mcont .owner_panel a img")
         if photo:
