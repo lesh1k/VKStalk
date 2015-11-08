c6808b7ebf2d05c0b574ec2dab50703d3ef19df7
Alexei Dvorac <lexhxas@gmail.com>
2015-11-08 21:42:54 +0200

    Add summary flag and related options (max-days, summary-to-file, summary-to-console) to argparse.

--- src/helpers/h_argparse.py
+++ src/helpers/h_argparse.py
@@ -1,19 +1,49 @@
-from config.settings import PROJECT_NAME
+from config import settings
+from core.summarizer import write_summary
 
 import argparse
 
 
 def get_args():
-    parser = argparse.ArgumentParser(prog=PROJECT_NAME)
+    parser = argparse.ArgumentParser(prog=settings.PROJECT_NAME)
     parser.add_argument(
         'vk_id',
         type=str,
         help='String. User VK id (e.g. 45156687 OR alexei.dvorac)',
     )
-    # parser.add_argument('-d', '--debug',
-    #                     type=bool,
-    #                     dest='debug',
-    #                     help="Boolean. Enable/disable logging debug messages."
-    #                     )
+    parser.add_argument(
+        '-s', '--summary',
+        action='store_const',
+        const=True,
+        dest='summary',
+        help="This flag triggers summary generation."
+    )
+    parser.add_argument(
+        '-md', '--max-days',
+        type=int,
+        default=settings.DEFAULT_SUMMARY_PERIOD,
+        dest='summary_max_days',
+        help="int, max days range for summary. DEFAULT: %(default)s"
+    )
+    parser.add_argument(
+        '-stf', '--summary-to-file',
+        type=bool,
+        default=True,
+        dest='summary_to_file',
+        help="if True, summary will be written to file. DEFAULT: %(default)s"
+    )
+    parser.add_argument(
+        '-stc', '--summary-to-console',
+        type=bool,
+        default=True,
+        dest='summary_to_console',
+        help="if True, summary will be written to console. DEFAULT: %(default)s"
+    )
+    parser.add_argument(
+        '-v', '--version',
+        action='version',
+        version='%(prog)s {}'.format(settings.VERSION),
+        help="Print program version"
+    )
     args = parser.parse_args()
     return args
--- src/main.py
+++ src/main.py
@@ -1,11 +1,19 @@
 from core.vkstalk import VKStalk
-import sys
-import re
 from helpers.h_logging import setup_logging
 from helpers.h_argparse import get_args
+from core.summarizer import write_summary
+
+import sys
+import re
+
 
 if __name__ == "__main__":
     cli_args = vars(get_args())
     setup_logging(cli_args['vk_id'])
+    if cli_args['summary']:
+        write_summary(cli_args['vk_id'], max_days=cli_args['summary_max_days'],
+                      to_file=cli_args['summary_to_file'],
+                      to_console=cli_args['summary_to_console'])
+        exit()
     vk_object = VKStalk(cli_args['vk_id'])
     vk_object.scrape()
