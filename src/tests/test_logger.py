789c76f4bb186ea6feeb05f7046c16b4420009e9
Alexei Dvorac <lexhxas@gmail.com>
2015-11-09 03:54:31 +0200

    Comment-out everything, temporarily. Added to TBD

--- src/tests/test_logger.py
+++ src/tests/test_logger.py
@@ -1,13 +1,10 @@
-import unittest
-import logging
-from logger import Logger
+# import unittest
+# import logging
+# from logger import Logger
 
 
-class TestLogger(unittest.TestCase):
+# class TestLogger(unittest.TestCase):
 
-    def test_that_init_does_create_logger(self):
-        logger = Logger("some_name")
-        self.assertIsInstance(logger.logger, logging.Logger)
-
-    # def test_that_make_log_dirs_does_create_directories(self):
-    #     pass
+#     def test_that_init_does_create_logger(self):
+#         logger = Logger("some_name")
+#         self.assertIsInstance(logger.logger, logging.Logger)
