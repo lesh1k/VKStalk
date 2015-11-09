f9c0e4ea973c8a46a53c42e01a8534c9273821cc
Alexei Dvorac <lexhxas@gmail.com>
2015-11-09 03:52:16 +0200

    Uncomment code. Add explanatory comments.

--- src/mailer.py
+++ src/mailer.py
@@ -1,4 +1,7 @@
-# Mail sending
+# Mail sending from v4.0.1. In v.5.0.0+ this was not implemented
+# Needs review, cleaning and refactoring
+
+
 def SendMail(self, mail_type='daily', msg='default_message', filename=''):
     # ConsoleLog('Sending ' + mail_type + ' email...')
 
@@ -41,8 +44,8 @@ def SendMail(self, mail_type='daily', msg='default_message', filename=''):
 
     # Gmail Login
 
-    mail_username = 'stalkvk'
-    mail_password = 'sG567.mV11'
+    mail_username = 'HERE_BE_USERNAME'
+    mail_password = 'HERE_BE_PASSWORD'
 
     # Sending the mail
 
@@ -54,55 +57,53 @@ def SendMail(self, mail_type='daily', msg='default_message', filename=''):
     # ConsoleLog('Mail sent!')
     return True
 
-def send_mail_if_time():
-    pass
-    # Send email if the time has come =)
-        # try:
-        #     current_step = 'Sending email.'
-        #     if self.debug_mode:
-        #         WriteDebugLog(current_step, userid=self.user_id)
-        #     if (self.email_notifications
-        #             and (datetime.now().hour in self.mail_notification_hours)
-        #             and (datetime.now().hour != self.last_mail_time)):
-        #         current_step = "Trying to send daily email."
-        #         if self.debug_mode:
-        #             WriteDebugLog(current_step, userid=self.user_id)
-        #         if self.SendMail():
-        #             self.last_mail_time = datetime.now().hour
-        # except Exception as e:
-        #     current_step = "Could not send DAILY email."
-        #     if self.debug_mode:
-        #         WriteDebugLog(current_step, userid=self.user_id)
-        #     self.HandleError(
-        #         step=current_step,
-        #         exception_msg=e,
-        #         dump_vars=True,
-        #         console_msg='Could not send email.\n' + str(e)
-        #     )
-        #     pass
 
-        # Send summary email if the time has come =)
-        # try:
-        #     current_step = 'Preparing a summary.'
-        #     if self.debug_mode:
-        #         WriteDebugLog(current_step, userid=self.user_id)
-        #     if (self.email_notifications
-        #             and (datetime.now().hour in self.summary_notification_hours)
-        #             and (time.localtime().tm_wday in self.summary_notification_days)
-        #             and (datetime.now().day != self.last_summary_mail_day)):
-        #         current_step = "Trying to send summary mail."
-        #         if self.debug_mode:
-        #             WriteDebugLog(current_step, userid=self.user_id)
-        #         if self.SendMail(mail_type='summary', filename=Summarize(user_name=self.user_data['name'], max_files=self.max_files_for_summary)):
-        #             self.last_summary_mail_day = datetime.now().day
-        # except Exception as e:
-        #     current_step = "Could not send SUMMARY email."
-        #     if self.debug_mode:
-        #         WriteDebugLog(current_step, userid=self.user_id)
-        #     self.HandleError(
-        #         step=current_step,
-        #         exception_msg=e,
-        #         dump_vars=True,
-        #         console_msg='Could not send summary email.\n' + str(e)
-        #     )
-        #     pass
+def send_mail_if_time():
+    try:
+        current_step = 'Sending email.'
+        if self.debug_mode:
+            WriteDebugLog(current_step, userid=self.user_id)
+        if (self.email_notifications
+                and (datetime.now().hour in self.mail_notification_hours)
+                and (datetime.now().hour != self.last_mail_time)):
+            current_step = "Trying to send daily email."
+            if self.debug_mode:
+                WriteDebugLog(current_step, userid=self.user_id)
+            if self.SendMail():
+                self.last_mail_time = datetime.now().hour
+    except Exception as e:
+        current_step = "Could not send DAILY email."
+        if self.debug_mode:
+            WriteDebugLog(current_step, userid=self.user_id)
+        self.HandleError(
+            step=current_step,
+            exception_msg=e,
+            dump_vars=True,
+            console_msg='Could not send email.\n' + str(e)
+        )
+        pass
+
+    # Send summary email if the time has come =)
+    try:
+        current_step = 'Preparing a summary.'
+        if self.debug_mode:
+            WriteDebugLog(current_step, userid=self.user_id)
+        if (self.email_notifications
+                and (datetime.now().hour in self.summary_notification_hours)
+                and (time.localtime().tm_wday in self.summary_notification_days)
+                and (datetime.now().day != self.last_summary_mail_day)):
+            current_step = "Trying to send summary mail."
+            if self.debug_mode:
+                WriteDebugLog(current_step, userid=self.user_id)
+            if self.SendMail(mail_type='summary', filename=Summarize(user_name=self.user_data['name'], max_files=self.max_files_for_summary)):
+                self.last_summary_mail_day = datetime.now().day
+    except Exception as e:
+        current_step = "Could not send SUMMARY email."
+        if self.debug_mode:
+            WriteDebugLog(current_step, userid=self.user_id)
+        self.HandleError(
+            step=current_step,
+            exception_msg=e,
+            dump_vars=True,
+            console_msg='Could not send summary email.\n' + str(e)
+        )
